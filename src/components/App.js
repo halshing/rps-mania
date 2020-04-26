a22ec0be096340ddf32a56f70a6e0445e7df1209
Hasan Shingieti <hasan.alshingieti@gmail.com>
2020-04-26 15:18:50 -0400

    app created

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>RPS Mania App</h1>
+    </div>
+  );
+};
+
+export default App;
